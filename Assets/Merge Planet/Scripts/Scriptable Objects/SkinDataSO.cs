bf7f0a02f56f81a96e815ae40c86581b3312b3e1
sovainoiii <bechuotbi@gmail.com>
2024-05-12 21:35:05 +0700

    make menu create scriptable object for skin in editor

---
+++ Assets/Merge Planet/Scripts/Scriptable Objects/SkinDataSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Skin Data", menuName = "Scritpable Objects/Skin Data", order = 0)]
+public class SkinDataSO : MonoBehaviour
+{
+
+}
